53316cb9e7349a230994d16299d60fe2a53516cf
pivotal <pivotal@metson.sf.pivotallabs.com>
2014-08-25 16:51:58 -0700

    add diego1 and diego2 cf helpers

--- cf_helpers.bash
+++ cf_helpers.bash
@@ -1,5 +1,5 @@
 function cf_warden() {
-  echo "logging into warden"  
+  echo "logging into warden"
   cf api api.10.244.0.34.xip.io --skip-ssl-validation
   cf login -u admin -p admin -o onsi -s onsi
   cf create-org onsi
@@ -9,8 +9,28 @@ function cf_warden() {
 }
 
 function cf_ketchup() {
-  echo "logging into ketchup"  
+  echo "logging into ketchup"
   cf api api.ketchup.cf-app.com --skip-ssl-validation
+  cf login -u admin -p $KETCHUP_CF_PASSWORD -o onsi -s onsi
+  cf create-org onsi
+  cf target -o onsi
+  cf create-space onsi
+  cf target -s onsi
+}
+
+function cf_diego1() {
+  echo "logging into diego-1"
+  cf api api.diego-1.cf-app.com --skip-ssl-validation
+  cf login -u admin -p $KETCHUP_CF_PASSWORD -o onsi -s onsi
+  cf create-org onsi
+  cf target -o onsi
+  cf create-space onsi
+  cf target -s onsi
+}
+
+function cf_diego2() {
+  echo "logging into diego-2"
+  cf api api.diego-2.cf-app.com --skip-ssl-validation
   cf login -u admin -p $KETCHUP_CF_PASSWORD -o onsi -s onsi
   cf create-org onsi
   cf target -o onsi
