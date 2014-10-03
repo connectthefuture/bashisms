fb90f46a9644eda1e326c8a43e7d9ab74129c789
Onsi Fakhouri <onsijoe@gmail.com>
2014-10-02 17:51:38 -0700

    add a1

--- cf_helpers.bash
+++ cf_helpers.bash
@@ -18,6 +18,16 @@ function cf_ketchup() {
   cf target -s onsi
 }
 
+function cf_a1() {
+  echo "logging into ketchup"
+  cf api api.a1.cf-app.com --skip-ssl-validation
+  cf login -u admin -p $KETCHUP_CF_PASSWORD -o onsi -s onsi
+  cf create-org onsi
+  cf target -o onsi
+  cf create-space onsi
+  cf target -s onsi
+}
+
 function cf_diego1() {
   echo "logging into diego-1"
   cf api api.diego-1.cf-app.com --skip-ssl-validation
