03cfef9b137ee53919f898b34b656324fdbe8ad0
Onsi Fakhouri <onsijoe@gmail.com>
2014-11-13 15:23:43 -0800

    support diego edge envs

--- veritas.bash
+++ veritas.bash
@@ -20,6 +20,16 @@ function veritas_up() {
   echo "$bootstrap" | pbcopy
 }
 
+function veritas_edge() {
+  export LOGGREGATOR_ADDR=loggregator.192.168.11.11.xip.io
+  export DIEGO_RECEPTOR_ADDRESS=receptor.192.168.11.11.xip.io
+  export RECEPTOR_ENDPOINT=receptor.192.168.11.11.xip.io
+  export EXECUTOR_ADDR=192.168.11.11:1700
+  export ETCD_CLUSTER=http://192.168.11.11:4001
+  export GARDEN_ADDR=192.168.11.11:7777
+  export GARDEN_NETWORK="tcp"
+}
+
 function veritas_envs() {
   if [[ `get_bosh_target` != "warden" ]] ; then
     echo "You must use bosh cache and set the target to 'warden'"
