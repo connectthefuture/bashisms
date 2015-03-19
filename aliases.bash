542cf902fbc1963858b0a0f791a13a1d16ff9697
Onsi Fakhouri <onsijoe@gmail.com>
2015-03-19 10:15:01 -0700

    oh

--- aliases.bash
+++ aliases.bash
@@ -1,3 +1,4 @@
 alias gst="git status"
 alias reginkgo="go install github.com/onsi/ginkgo/ginkgo"
 alias diego="cd ~/workspace/diego-release"
+alias bosh="BUNDLE_GEMFILE=$HOME/.bashisms/bosh.Gemfile bundle exec bosh"
